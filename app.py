8774c03adfd21c8c0856476a3b09f69fe6aacd5c
yejinnim <yeding1720@gmail.com>
2024-08-16 11:58:05 +0900

    주석 달고, 실행 오류 코드 수정함. but 아직 error 해결 못함.

--- app.py
+++ app.py
@@ -29,8 +29,11 @@ def demo_process(input_img):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    # `task`: 모델이 특정 작업을 수행할 때 사용하는 데이터셋의 이름
     parser.add_argument("--task", type=str, default="docvqa")
+    # `pretrained_path`: Hugging Face Model Hub에서 제공되는 모델 경로임. 보통 `organization/repository` 형식임. 
     parser.add_argument("--pretrained_path", type=str, default="naver-clova-ix/donut-base-finetuned-docvqa")
+    # `port`, `url`, `sample_img_path`은 굳이 입력하지 않아도 기본값으로 실행됨
     parser.add_argument("--port", type=int, default=None)
     parser.add_argument("--url", type=str, default=None)
     parser.add_argument("--sample_img_path", type=str)
@@ -46,7 +49,9 @@ if __name__ == "__main__":
     if args.sample_img_path:
         example_sample.append(args.sample_img_path)
 
-    pretrained_model = DonutModel.from_pretrained(args.pretrained_path)
+    # CORD(document parsing)으로 실행 중 오류 발생하여 수정함 but url에서 이미지 업로드하면 error 발생함. 이유는 해결 못함.
+    pretrained_model = DonutModel.from_pretrained(args.pretrained_path, ignore_mismatched_sizes=True)
+    # default) pretrained_model = DonutModel.from_pretrained(args.pretrained_path)
 
     if torch.cuda.is_available():
         pretrained_model.half()
